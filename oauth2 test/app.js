6417336e64e96b81a7dc028a20edaea836cda7ad
adrian.frimpong <adrian.frimpong@gmail.com>
2014-02-05 13:26:36 -0500

    adding all hack files to github for sharing

--- oauth2 test/app.js
+++ oauth2 test/app.js
@@ -50,34 +50,44 @@ $(function() {
     parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
     console.log(parameterMap);
 
+    var delay = 2000;
     var geocoder;
     var map;
     function initialize() {
         geocoder = new google.maps.Geocoder();
-        var latlng = new google.maps.LatLng(-34.397, 150.644);
-        var mapOptions = {
-        zoom: 8,
-        center: latlng
-        }
+        // var latlng = new google.maps.LatLng(-34.397, 150.644);
+        // var mapOptions = {
+        // zoom: 8,
+        // center: latlng
+        // }
         // map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     }
 
     function codeAddress(address) {
         /* TODO: assign address var to business variables */
         var address = address;
-        geocoder.geocode( { 'address': address}, function(results, status) {
-            
-            /*marker for google maps*/
-            if (status == google.maps.GeocoderStatus.OK) {
-                map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: results[0].geometry.location
-                });
-            } else {
-                alert("Geocode was not successful for the following reason: " + status);
-            }
-        });
+        
+            return geocoder.geocode( { 'address': address}, function(results, status) {
+                
+                /*marker for google maps*/
+                if (status == google.maps.GeocoderStatus.OK) {
+                    
+                        console.log("called it!");
+                        return results[0].geometry.location
+                
+                
+                /* The below will be incorporated  */
+                // map.setCenter(results[0].geometry.location);
+                // var marker = new google.maps.Marker({
+                //     map: map,
+                //     position: results[0].geometry.location
+                // });
+                } else {
+                    alert("Geocode was not successful for the following reason: " + status);
+                }
+            });
+
+        
     }
     /*actual callback function that displays image property of first yelp business returned from search query*/
     function cb (data) {
@@ -87,14 +97,17 @@ $(function() {
             // assign local variables address and city for geolocation
             var address = data.businesses[i].location.address[0];
             var city = data.businesses[i].location.city
-
-            $('#foo').append('<p><img src=' + 
-                data.businesses[i].image_url + ' alt="yelp photo">' + 
-                address + ' ' + 
-                data.businesses[i].location.city + ' ' +
-                data.businesses[i].location.country_code + 
-                codeAddress()
-                 ' </p>');
+            var country = data.businesses[i].location.country_code;
+            
+            setTimeout(function(data, address, city, country){
+                $('#foo').append('<p><img src=' + 
+                    data.businesses[i].image_url + ' alt="yelp photo">' + 
+                    address + ' ' + 
+                    city + ' ' +
+                    country + ' ' +
+                    codeAddress(address+' '+city+' '+ country) + ' ' +
+                     ' </p>');
+            }, delay);
         }
          // $('#foo').append('<img src=' + data.businesses[0].image_url + ' alt="yelp photo" height="250">');
          // console.log("hi");
@@ -109,6 +122,7 @@ $(function() {
     	dataType: 'jsonp',
     	// jsonpCallback: 'cb',
     	success: function(data, textStats, XMLHttpRequest) {
+            initialize();
             cb(data);
             console.log(data);
     		console.log(data.businesses[0].image_url);
